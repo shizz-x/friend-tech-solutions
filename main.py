533c0e077e0c857998b3ea7096b9faca29f84d4e
shizz-x <71591110+shizz-x@users.noreply.github.com>
2023-10-16 21:52:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-import sys
 import dotenv 
 from modules.utils import Utility as utils
 utils.read_environ()
 dotenv.load_dotenv()
 from modules.core import Core
-from modules.friendclient import Client
 
 
 
 
 
-def main(args: list[str] = sys.argv):
+def main():
     core = Core()
 
     core.start_procces()
